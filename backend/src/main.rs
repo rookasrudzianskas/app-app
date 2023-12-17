3f1668aa0a7a761c9bf22f4f01c543ef64b5bb88
Rokas <rookas.rudzenskas@gmail.com>
2023-12-17 10:39:43 +0200

    fix

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,6 @@
-fn main() {
-    println!("Hello, world!");
-}
+use postgres::{ Client, NoTls };
+use postgres::Error as PostgresError;
+use std::net::{ TcpListener, TcpStream };
+use std::io::{ Read, Write };
+use std::env;
+
