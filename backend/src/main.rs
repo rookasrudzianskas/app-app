58801b4ae6583fe999401a23ce39c3306767e88a
Rokas <rookas.rudzenskas@gmail.com>
2023-12-17 10:42:16 +0200

    define constants

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -4,3 +4,22 @@ use std::net::{ TcpListener, TcpStream };
 use std::io::{ Read, Write };
 use std::env;
 
+#[macro_use]
+extern crate serde_derive;
+
+//Model: User struct with id, name, email
+#[derive(Serialize, Deserialize)]
+struct User {
+    id: Option<i32>,
+    name: String,
+    email: String,
+}
+
+//DATABASE URL
+const DB_URL: &str = env!("DATABASE_URL");
+
+//constants
+const OK_RESPONSE: &str =
+    "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\nAccess-Control-Allow-Origin: *\r\nAccess-Control-Allow-Methods: GET, POST, PUT, DELETE\r\nAccess-Control-Allow-Headers: Content-Type\r\n\r\n";
+const NOT_FOUND: &str = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+const INTERNAL_ERROR: &str = "HTTP/1.1 500 INTERNAL ERROR\r\n\r\n";
